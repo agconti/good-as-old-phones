a23910ab6f36d3d18184c0d0aceb73d14b6eb8b0
agconti <agc11d@gmail.com>
2016-01-14 14:12:31 -0500

    feat(ContactsView): added contacts information

---
+++ GoodAsOldPhones/ContactViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ContactViewController.swift
+//  GoodAsOldPhones
+//
+//  Created by Andrew Conti on 1/14/16.
+//  Copyright © 2016 agconit. All rights reserved.
+//
+
+import UIKit
+
+class ContactViewController: UIViewController {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.addSubview(scrollView)
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        scrollView.contentSize = CGSizeMake(375, 1000)
+    }
+
+}
